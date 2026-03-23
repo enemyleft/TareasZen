7fd7cc64d10df6acd44fcba330f9e2ec4c21563c
William Bettencourt <william.bettencourt@united.com>
2026-03-23 14:30:46 -0300

    preloading spanish translation

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
 
-import { messages } from "../locales/en/messages";
+import { messages as enMessages } from "../locales/en/messages";
+import { messages as esMessages } from "../locales/es/messages";
 
 import App from "./App";
 import "./styles.css";
 
-i18n.load("en", messages);
+i18n.load("en", enMessages);
+i18n.load("es", esMessages);
 i18n.activate("es");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
